d592a7bc6036fed6b08de5209d322fdaa37f7222
okicikk <145479970+okicikk@users.noreply.github.com>
2023-09-30 23:34:25 +0300

    ArraysLab

---
+++ 01. Day Of Week/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+namespace _01._Day_Of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = (Console.ReadLine());
+            BigInteger day = default;
+            if (char.IsDigit(command, 0))
+            {
+                day = BigInteger.Parse(command);
+            }
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            if (day < 1 || day > 7)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[(int)day - 1]);
+            }
+        }
+    }
+}
---
+++ 03. Rounding Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            int[] output = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                output[i] = (int)Math.Round(nums[i], MidpointRounding.AwayFromZero);
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine($"{nums[i]} => {output[i]}");
+            }
+        }
+    }
+}
